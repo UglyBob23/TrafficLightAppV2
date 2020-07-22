83ea37451156ceaeff60dcf00640ea57dd4e59e5
Vladimir <UglyBob23@yandex.ru>
2020-07-22 11:42:32 +0500

    Initial Commit

---
+++ TrafficLightAppV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightAppV2
+//
+//  Created by Владимир Паутов on 22.07.2020.
+//  Copyright © 2020 Владимир Паутов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
